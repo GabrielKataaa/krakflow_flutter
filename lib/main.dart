a3a92df6ad4e8594824dc26d46e8c4175b282d40
Gabriel Kata <gabriel.kata@o2.pl>
2026-03-24 19:08:14 +0100

    lab_4

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,72 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
+
+  List<Task> tasks = [
+    Task(title: "zrobic pranie ", deadline: "18:00"),
+    Task(title: "wyjsc z psem ", deadline: "22:45"),
+    Task(title: "nakarmic kota ", deadline: "23:00"),
+    Task(title: "umyc naczynia", deadline: "24:00"),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      home: Scaffold(
-       body: Center(
-          child: Column(
+      title: 'KrakFlow',
+      home: Center(
+        child: Column(
           children: [
-            Text("KrakFlow"),
-            Text("Organizacja studiow"),
-            Text("Dzisiejsze zadania"),
-        ],
-      )))
+            Text(
+              "Dzisiejsze zadania",
+              style: TextStyle(fontSize: 67, fontWeight: FontWeight.bold),
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: tasks.length,
+                itemBuilder: (context, index) {
+                  return TaskCard(title: "${title}",);
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
 
+class Task {
+  final String title;
+  final String deadline;
+
+  Task({required this.title, required this.deadline});
+}
+
+class TaskCard extends StatelessWidget {
+  final String title;
+  final String subtitle;
+  final IconData icon;
+
+  const TaskCard({
+    super.key,
+    required this.title,
+    required this.subtitle,
+    required this.icon,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: Icon(icon),
+        title: Text(title),
+        subtitle: Text(subtitle),
+      ),
+    );
+  }
+}
